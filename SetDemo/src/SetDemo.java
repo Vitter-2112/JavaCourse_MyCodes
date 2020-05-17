f79319f0b3c272bf9414c6dadeb554a557557703
Vitter-2112 <goryt@web.de>
2020-05-17 20:25:50 +0200

    Mnozhestwa_nomer1_HA

---
+++ SetDemo/src/SetDemo.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+
+public class SetDemo {
+    public static void main(String[] args) {
+
+    LinkedList<String> list=new LinkedList<>();
+        list.add("Petr");
+        list.add("Anna");
+        list.add("Maria");
+        list.add("Ivan");
+        list.add("Anna");
+        list.add("Petr");
+        list.add("Filip");
+        list.add("Anna");
+        list.add("George");
+
+        //System.out.println(list);
+        Set<String> set= new HashSet<>();
+
+        set.addAll(list);// Set allows no duplicates, consequently they will be eliminated by adding thm to a set
+        System.out.println(set);
+
+} }
